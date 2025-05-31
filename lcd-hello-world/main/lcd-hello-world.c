7dd34554bba7a46c21c9138a9a56f993314bdc4f
Amelia Maclatchy <amelia.maclatchy@gmail.com>
2025-05-31 08:07:33 -0400

    [lcd-hello-world] Init project creation

---
+++ lcd-hello-world/main/lcd-hello-world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+#include "ssd1306.h"
+
+void app_main(void)
+{
+
+}
